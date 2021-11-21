cf04793f75c242a33a9a085ffbc14211af2b4b1c
Aneet Narendranath <dnaneet@mtu.edu>
2021-11-21 13:14:50 -0500

    feeding chart.

--- baby.py
+++ baby.py
@@ -38,7 +38,7 @@ st.markdown("### Feeding Pattern")
 st.table(df["Feeding"])
 fig_feed = px.scatter(df[df["Feeding"]!="No"], x = "Timestamp", y = "Feeding", hover_data = ["Feeding Volume [Oz] (approximate)", "Feeding duration (minutes)"]) #, size = "Enrollment", color = "Course"
 fig_feed.update_yaxes(range=[0,3])
-#     st.plotly_chart(fig)
+st.plotly_chart(fig)
 st.plotly_chart(fig_feed)
 
 st.markdown("### Pumping Pattern")
