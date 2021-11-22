e830d60a1af29b7f786eda7ad0bca907818f2016
Aneet D Narendranath, PhD <dnaneet@mtu.edu>
2021-11-22 09:09:36 -0500

    new spreadsheet model.

--- baby.py
+++ baby.py
@@ -8,23 +8,25 @@ Original file is located at
 """
 
 import pandas as pd
-import plotly.express as px
 import numpy as np
+import plotly.express as px
+import datetime
+import seaborn as sns
 import streamlit as st
 
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTKvzS3e40jAllf6kSFZ00ewJAPnftHPd_GuyRhoG-RNBbzTbexW_tZWNF38AvnQX-sk8Yfd1JAZksf/pub?gid=63392174&single=true&output=csv" 
 df = pd.read_csv(url)
 
-#df.head(1)
-df["Pump?"] = df["Pump?"].fillna("No")
+#df["Pump?"] = df["Pump?"].fillna("No")
 df["Pump duration (minutes)"] = df["Pump duration (minutes)"].fillna(0)
-df["Feeding"] = df["Feeding"].fillna("No")
+df["Feeding"] = df["Feeding"].fillna("not reported")
 df["Feeding Volume [Oz] (approximate)"] = df["Feeding Volume [Oz] (approximate)"].fillna("not reported")
 df["Feeding duration (minutes)"] = df["Feeding duration (minutes)"].fillna(0)
 df["Diaper"] = df["Diaper"].fillna("not reported")
 df["Most recently used breast"] = df["Most recently used breast"].fillna("not reported")
+#df["date"] = pd.to_datetime(df['Timestamp']).apply(lambda x: x.date())
 df["date"] = pd.to_datetime(df['Timestamp']).apply(lambda x: x.date())
-df["time"] = pd.to_datetime(df['Timestamp']).apply(lambda x: str(x.time()))
+#df["time"] = pd.to_datetime(df['Feeding start time']).apply(lambda x: x.time())
 
 
 #df["date"].tail(1)
@@ -32,28 +34,28 @@ df["time"] = pd.to_datetime(df['Timestamp']).apply(lambda x: str(x.time()))
 
 
 st.title("Baby Reporting")
-selection = st.sidebar.radio('Select ', ["Charts", "Tables"])
+selection = st.sidebar.radio('Select ', ["Tables", "Charts"])
 
 
 if selection == "Charts":
-    st.markdown("Baby data is reported via charts on this page.")
-    st.markdown("### Feeding Pattern")
+    #st.markdown("Baby data is reported via charts on this page.")
+    #st.markdown("### Feeding Pattern")
     #st.table(df["Feeding"])
 
-    fig = px.scatter(df[df["Feeding"]!="No"], x = "Timestamp", y = "Feeding", hover_data = ["Feeding Volume [Oz] (approximate)", "Feeding duration (minutes)"]) #, size = "Enrollment", color = "Course"
-    fig.update_yaxes(range=[0,3])
-    st.plotly_chart(fig)
+    #fig = px.scatter(df[df["Feeding"]!="No"], x = "Timestamp", y = "Feeding", hover_data = ["Feeding Volume [Oz] (approximate)", "Feeding duration (minutes)"]) #, size = "Enrollment", color = "Course"
+    #fig.update_yaxes(range=[0,3])
+    #st.plotly_chart(fig)
 
-    st.markdown("### Diaper Pattern")
+    #st.markdown("### Diaper Pattern")
     #st.table(df["Pump?"])
-    fig = px.scatter(df, x = "Timestamp", y = "Diaper", hover_data = ["Diaper",]) #, size = "Enrollment", color = "Course"
-    fig.update_yaxes(range=[0,3])
-    st.plotly_chart(fig)
+    #fig = px.scatter(df, x = "Timestamp", y = "Diaper", hover_data = ["Diaper",]) #, size = "Enrollment", color = "Course"
+    #fig.update_yaxes(range=[0,3])
+    #st.plotly_chart(fig)
 if selection == "Tables":
     st.markdown("Baby data is reported via tables on this page.")
     st.markdown("### Feeding Pattern")
-    st.table(df[["date", "time", "Feeding"]])
+    st.table(df[["date", "Feeding start time", "Feeding"]])
 
     st.markdown("### Diaper Pattern")
-    st.table(df[["date", "time", "Diaper"]])
+    st.table(df[["date", "Diaper change time", "Diaper"]])
 
